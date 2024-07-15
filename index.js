a8386ffb31b7aa88c6eaf2e9e521945823425765
swapnil khare <swapnilk@codiant.com>
2024-07-15 16:47:26 +0530

    fix file upload code and setup with AWS S3

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+import dotenv from 'dotenv'
 import express from 'express';
 import mongoose from 'mongoose';
+import multer from 'multer';
+import multerS3 from 'multer-s3-transform';
+import AWS from 'aws-sdk';
 
 // Initialize the app
 const app = express();
 
+// Initiate env
+dotenv.config();
+
 // Middleware to parse JSON
 app.use(express.json());
 
-// MongoDB connection string
-const dbURI = 'mongodb+srv://swapnilkhare2990:cdY79gYxheAgw7aS@cluster0.ahhasfr.mongodb.net/';
+/****************************** Connect to MongoDB **********************************/
 
-// Connect to MongoDB
+// MongoDB connection string
+const dbURI = process.env.DATABASE_URL;
+
 mongoose.connect(dbURI)
   .then(() => app.listen(3000, () => console.log('Server is running on port 3000')))
   .catch(err => console.log(err));
@@ -25,9 +33,67 @@ const postSchema = new mongoose.Schema({
 
 const Post = mongoose.model('Post', postSchema);
 
-// Define routes
+/****************** Configure AWS with your access and secret key. *******************/
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID, // or your access key here
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, // or your secret key here
+  region: process.env.AWS_REGION, // or your desired region here
+});
+
+let storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/')  // Destination folder for uploaded files
+  },
+  filename: function(req, file, cb) {
+    cb(null, `${Date.now().toString()}-${file.originalname}`);
+  }
+});
+
+// Set storage engine
+if(process.env.STORAGE_TYPE === 's3'){
+  storage = multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET_NAME,
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
+      const fileName = `${Date.now().toString()}-${file.originalname}`;
+      cb(null, fileName);
+    },
+    contentType: (req, file, cb) => {
+      cb(null, file?.mimetype);
+    },
+    ACL(req, file, cb) {
+      const acl = 'public-read';
+      cb(null, acl);
+    },
+  });
+}
+
+// Set up multer S3 storage engine
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 }, // File size limit (optional)
+});
+
+/********************************** Define routes *************************************/
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+// Single file upload endpoint
+app.post('/upload', upload.single('file'), (req, res) => {
+  try {
+    res.send({
+      message: 'File uploaded successfully',
+      file: req.file,
+    });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 // CRUD API routes
@@ -38,7 +104,7 @@ app.post('/posts', async (req, res) => {
     const result = await post.save();
     res.send(result);
   } catch (err) {
-    res.status(400).send(err);
+    res.status(500).send(err);
   }
 });
 
@@ -48,7 +114,7 @@ app.get('/posts', async (req, res) => {
     const posts = await Post.find();
     res.send(posts);
   } catch (err) {
-    res.status(400).send(err);
+    res.status(500).send(err);
   }
 });
 
@@ -62,7 +128,7 @@ app.get('/posts/:id', async (req, res) => {
       res.status(404).send('Post not found');
     }
   } catch (err) {
-    res.status(400).send(err);
+    res.status(500).send(err);
   }
 });
 
@@ -76,7 +142,7 @@ app.put('/posts/:id', async (req, res) => {
       res.status(404).send('Post not found');
     }
   } catch (err) {
-    res.status(400).send(err);
+    res.status(500).send(err);
   }
 });
 
@@ -90,6 +156,6 @@ app.delete('/posts/:id', async (req, res) => {
       res.status(404).send('Post not found');
     }
   } catch (err) {
-    res.status(400).send(err);
+    res.status(500).send(err);
   }
 });
